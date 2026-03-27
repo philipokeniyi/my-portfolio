a1fd2f55403ef4179a8cf77856256e91d113fc42
Philip Okeniyi <okeniyiphilip@gmail.com>
2026-03-27 13:30:56 +0100

    background added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,14 +2,38 @@ import React from "react";
 import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import ShapeGrid from "./components/ShapeGrid";
 
 function Layout() {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          zIndex: 0,
+          opacity: 0.3,
+        }}
+      >
+        <ShapeGrid
+          speed={0.5}
+          squareSize={40}
+          direction="diagonal"
+          borderColor="#271E37"
+          hoverFillColor="#222222"
+          shape="square"
+          hoverTrailAmount={0}
+        />
+      </div>
+      <div style={{ position: "relative", zIndex: 10 }}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
